2018cb9e0a9ca7fafc03122c3109aa3b6ee40f90
hongjaewon62 <hongjaewon0602@gmail.com>
2025-02-07 23:53:32 +0900

    Notice 추가중

--- src/Content/NoticeContent.jsx
+++ src/Content/NoticeContent.jsx
@@ -36,12 +36,47 @@ const ContentNoticeWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    border-bottom: 1px solid #f4f4f4;
 `
 
 const NoticeTitle = styled.strong`
     color: #222222;
     font-size: 16px;
 `
+
+const NoticeLinkBtn = styled.a`
+    justify-content: center;
+    align-items: center;
+    color: #222222;
+    font-size: 14px;;
+    width: 300px;
+    height: 20px;
+    margin: 2px 0 0 28px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+
+    &:hover {
+        text-decoration: underline; 
+        cursor: pointer;
+    }
+`
+
+const MoreButton = styled.div`
+    padding: 2px 15px 3px;
+    font-weight: 400;
+    font-size: 12px;
+    color: #666;
+    border: 1px solid #f4f4f4;
+    border-radius: 11px;
+    cursor: pointer;
+`;
+
+const ClientWrapper = styled.div`
+    padding: 19px 0 22px;
+    display: flex;
+    justify-content: center;
+`;
 
 function NoticeContent() {
     return (
@@ -50,7 +85,12 @@ function NoticeContent() {
                 <NoticeClientWrapper>
                     <ContentNoticeWrapper>
                         <NoticeTitle>공지사항</NoticeTitle>
+                        <NoticeLinkBtn>[시스템 점검]iOS 18 업데이트 관련 예매 서비스 이용 안내</NoticeLinkBtn>
+                        <MoreButton>더보기</MoreButton>
                     </ContentNoticeWrapper>
+                    <ClientWrapper>
+                    <NoticeTitle>고객센터</NoticeTitle>
+                    </ClientWrapper>
                 </NoticeClientWrapper>
             </ContentWrapper>
         </Wrapper>
